b8f5b0df8ff36b30eb3b3a8aefe66efca154198c
jjime51 <100853086+jjime51@users.noreply.github.com>
2022-04-01 21:19:17 -0500

    Build qa

--- build/resources/test/features/prueba.feature
+++
@@ -1,6 +0,0 @@
-Feature: Como usuario quiero ingresar a la aplicacion
-
-  Scenario: Colocar los datos de logeo
-    Given Ingreso a la pagina web
-    When Ingreso los siguientes datos solicitados
-    Then Veo la pantalla principal
--- src/test/java/stepdefinitions/pruebaStep.java
+++ src/test/java/stepdefinitions/pruebaStep.java
@@ -4,7 +4,6 @@ import io.cucumber.java.Before;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import net.serenitybdd.screenplay.abilities.BrowseTheWeb;
 import net.serenitybdd.screenplay.actions.Open;
 import net.serenitybdd.screenplay.actors.OnStage;
 import net.serenitybdd.screenplay.actors.OnlineCast;
@@ -12,7 +11,6 @@ import questions.validar;
 import task.pruebaTask;
 import utils.SeleniumDemo;
 
-import static net.serenitybdd.screenplay.actors.OnStage.theActorCalled;
 import static net.serenitybdd.screenplay.actors.OnStage.theActorInTheSpotlight;
 
 public class pruebaStep {
@@ -29,9 +27,7 @@ public class pruebaStep {
     @Given("Ingreso a la pagina web")
     public void ingresoALaPaginaWeb() {
 
-        String actorName = "Juan";
-        String url = "https://www.saucedemo.com/";
-        theActorCalled(actorName).whoCan(BrowseTheWeb.with(SeleniumDemo.inThePage(url)));
+        theActorInTheSpotlight().wasAbleTo(Open.url("https://www.saucedemo.com/"));
     }
     @When("Ingreso los siguientes datos solicitados")
     public void ingresoLosSiguientesDatosSolicitados() {
