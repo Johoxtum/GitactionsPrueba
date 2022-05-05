0bba416b491be0d2de86f9654839e93e321bf938
jjime51 <100853086+jjime51@users.noreply.github.com>
2022-05-05 10:53:33 -0500

    prueba gitActions

--- shell/InstallChrome.sh
+++ shell/InstallChrome.sh
@@ -2,3 +2,7 @@
 set -ex
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo apt install ./google-chrome-stable_current_amd64.deb
+
+#zap
+docker run --name zap --rm -d -v $(pwd)/out:/owasp/zap -p 8080:8080 -i owasp/zap2docker-stable zap-x.sh -daemon -host 0.0.0.0 -port 8080 -config api.disablekey=true -config api.addrs.addr.name=.* -config api.addrs.addr.regex=true || exit 1
+docker logs -f zap &
